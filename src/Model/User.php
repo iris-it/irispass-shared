18d40327cd56d4a20d20ce5398e90f57dea802ee
Alexandre <alexandre.mangin@viacesi.fr>
2016-06-24 06:28:44 +0900

    remove sub

--- src/Model/User.php
+++ src/Model/User.php
@@ -6,8 +6,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-
-    protected $primaryKey = 'sub';
+    
     /**
      * The attributes that are mass assignable.
      *
